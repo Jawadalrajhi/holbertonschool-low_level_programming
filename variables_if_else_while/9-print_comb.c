18a66c4d8937e9cc73f987dffb3b855cb42e4ae1
root <root@ip-10-42-160-55.ec2.internal>
2025-08-29 19:09:38 +0000

    update

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - Entry point
- * Description: print all small alphabeti in reverse with a for loop
- * Return: Always 0 (Success)
- */
+	* main - prints all possible combinations of single-digit numbers
+	* Return: Always 0
+	*/
 int main(void)
 {
-int n;
-for (n = 'z'; n >= 'a'; n--)
-{
-putchar(n);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
