d43b84dbca46b85d3e0e0ff1fc3d9c2080997c93
root <root@ip-10-42-165-185.ec2.internal>
2025-09-04 15:50:58 +0000

    update

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,11 @@
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 return (c >= 'A' && c <= 'Z');
 }
+
