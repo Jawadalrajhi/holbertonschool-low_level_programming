035c77dfe1a603649d112fcce902f2768574a563
root <root@ip-10-42-165-185.ec2.internal>
2025-09-04 16:26:52 +0000

    update

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
  */
 void print_numbers(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i <= 9; i++)
-        _putchar('0' + i);
+for (i = 0; i <= 9; i++)
+_putchar('0' + i);
 
-    _putchar('\n');
+_putchar('\n');
 }
