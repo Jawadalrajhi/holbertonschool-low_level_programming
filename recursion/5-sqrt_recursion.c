8986d008d2665c34dfd42e01ca4fda6b01810b3e
root <root@ip-10-42-218-131.ec2.internal>
2025-09-08 18:35:16 +0000

    update

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,25 +1,19 @@
 #include "main.h"
 
-/**
- * _sqrt_recursion - returns natural square root of n
- * @n: number to calculate sqrt of
- *
- * Return: sqrt of n, -1 if n does not have a natural sqrt
- */
 int _sqrt_helper(int n, int i);
 
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-return _sqrt_helper(n, 0);
+    if (n < 0)
+        return (-1);
+    return (_sqrt_helper(n, 0));
 }
 
 int _sqrt_helper(int n, int i)
 {
-if (i * i == n)
-return (i);
-if (i * i > n)
-return (-1);
-return _sqrt_helper(n, i + 1);
+    if (i * i == n)
+        return (i);
+    if (i * i > n)
+        return (-1);
+    return (_sqrt_helper(n, i + 1));
 }
