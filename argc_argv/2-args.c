3d01783e941071fb03b107f8cc36dc87898c9e0f
root <root@ip-10-42-218-131.ec2.internal>
2025-09-08 18:58:46 +0000

    update

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+
+return (0);
+}
