078659b450c81ac860b9b233ee86a678b53f4758
root <root@ip-10-42-218-131.ec2.internal>
2025-09-08 18:28:18 +0000

    update

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: pointer to the string
+ * factorial - returns the factorial of a given number
+ * @n: number to calculate factorial of
  *
- * Return: length of the string
+ * Return: factorial of n, -1 if n < 0
  */
-int _strlen_recursion(char *s)
+int factorial(int n)
 {
-	if (*s == '\0')
-		return (0);
-
-	return (1 + _strlen_recursion(s + 1));
+    if (n < 0)
+        return (-1);
+    if (n == 0)
+        return (1);
+    return (n * factorial(n - 1));
 }
--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number
- * @n: number to calculate factorial
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base
+ * @y: exponent
  *
- * Return: factorial of n, -1 if n < 0
+ * Return: result of x^y, -1 if y < 0
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
-		return (-1);
-
-	if (n == 0)
-		return (1);
-
-	return (n * factorial(n - 1));
+    if (y < 0)
+        return (-1);
+    if (y == 0)
+        return (1);
+    return (x * _pow_recursion(x, y - 1));
 }
