aa202b3f5af1d86d98f580ee4c959c059d025941
root <root@ip-10-42-218-131.ec2.internal>
2025-09-08 17:54:27 +0000

    update

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
+while (*s)
+{
+if (*s == c)
+return (s);
+s++;
+}
 
-    if (c == '\0')  /* handle searching for '\0' */
-        return (s);
+if (c == '\0')  /* handle searching for '\0' */
+return (s);
 
-    return (NULL);
+return (NULL);
 }
