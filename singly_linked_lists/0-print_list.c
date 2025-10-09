93674251f1385ef42bde6cafb4d185f649aff762
root <root@ip-10-42-168-0.ec2.internal>
2025-10-09 08:46:12 +0000

    update

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,16 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t counter = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[%d] (%s)\n", 0, "nil");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		counter++;
-	}
-	return (counter);
+size_t counter = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[%d] (%s)\n", 0, "nil");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+counter++;
+}
+return (counter);
 }
