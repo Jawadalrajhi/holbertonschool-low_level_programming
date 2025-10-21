73e5f994de01332b7a3a8e839a68f9ebd37cbd54
root <root@ip-10-42-150-138.ec2.internal>
2025-10-21 17:51:59 +0000

    update

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,29 +1,32 @@
 #include "hash_tables.h"
+#include <stdio.h>
 
 /**
- * hash_table_get - Retrieve the value associated with
- *                  a key in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to get the value of.
- *
- * Return: If the key cannot be matched - NULL.
- *         Otherwise - the value associated with key in ht.
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
  */
-char *hash_table_get(const hash_table_t *ht, const char *key)
+void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int index;
+    unsigned long int i;
+    hash_node_t *node;
+    int first = 1;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
+    if (ht == NULL)
+        return;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
-
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
-	return ((node == NULL) ? NULL : node->value);
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            if (!first)
+                printf(", ");
+            printf("'%s': '%s'", node->key, node->value);
+            first = 0;
+            node = node->next;
+        }
+    }
+    printf("}\n");
 }
+
